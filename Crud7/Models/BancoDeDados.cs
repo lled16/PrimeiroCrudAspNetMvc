2af8ebffe386a45ff8678e3e8600d51cf69b51d7
João <vitoralves1604@gmail.com>
2022-03-10 23:46:16 -0300

    Primeiro CRUD com ASP NET MVC

---
+++ Crud7/Models/BancoDeDados.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud7.Models
+{
+    public class BancoDeDados : DbContext
+    {
+
+        public DbSet <Cliente> Clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Crud7;Integrated Security=True");
+        }
+    }
+}
